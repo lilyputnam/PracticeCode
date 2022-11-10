41456ee78746cc981bff0d98aedb6e014dbef1e9
lilyputnam <114833253+lilyputnam@users.noreply.github.com>
2022-11-11 10:35:05 +1300

    Update quickquiz.py

--- quickquiz.py
+++ quickquiz.py
@@ -18,12 +18,16 @@ if answer.lower() == "blue":
 else:
     print("Incorrect!")
 
+#learning how to increment number to keep score
+
 answer = input("What is the capital of New Zealand? ")
 if answer.lower() == "wellington":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
+
+#using .lower() is good to use in case they type the answer correctly but in capitals
 
 answer = input("How many days of the year are there? ")
 if answer == "365":
@@ -42,7 +46,7 @@ else:
 print("You got " + str(score) + " questions correct!")
 print("You got " + str((score/4)*100) + "%")
 
-
+#calculating a percentage
 
 
 
