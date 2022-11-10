19d289c1a8d5bdaeb1d0ccda4360ca5e85d7f952
lilyputnam <114833253+lilyputnam@users.noreply.github.com>
2022-11-10 14:23:32 +1300

    Add files via upload

---
+++ passwordproject.py
@@ -0,0 +1,29 @@
+#This is a program that allows you to store your passwords
+
+master_password = input("What is the master password? ")
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            print(line.rstrip())
+            user, passw = data.split("|")
+            print("User:", user, ", Password:", passw)
+
+def add():
+    name = input("Account name: ")
+    password = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + password + "\n")
+
+while True:
+    choice = input("Would you like to add a new password or view existing ones (view, add), press q to quit ").lower()
+    if choice == "q":
+        break
+
+    elif choice == "view":
+        view()
+    elif choice == "add":
+        add()
+    else:
+        print("Invalid choice.")
